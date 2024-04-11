644c692b8f5b5f5d52104e6cab639a1517d7b9df
ProtoJanusLabs <165827599+ProtoJanusLabs@users.noreply.github.com>
2024-04-11 12:52:22 -0500

    Create functions for GameController and Gameboard

---
+++ index.js
@@ -0,0 +1,45 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+
+    const board = [];
+
+    const createBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(" ");
+            }
+        }
+    }
+
+    const log = () => {
+        console.table(board);
+    }
+
+    return {createBoard, log};
+}
+
+function GameController() {
+    const players = [
+        {
+            name: "Player One",
+            token: "X"
+        },
+        {
+            name: "Player Two",
+            token: "O"
+        }
+    ];
+
+    const board = Gameboard();
+    board.createBoard();
+
+
+
+}
+
+const board = Gameboard();
+
+board.log();
+
