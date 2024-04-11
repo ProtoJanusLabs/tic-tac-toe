ac93d31fab4b0e462a874604a0857d26067452d2
ProtoJanusLabs <165827599+ProtoJanusLabs@users.noreply.github.com>
2024-04-11 13:17:18 -0500

    Add prettier as dependancy and run on index.js

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 function Gameboard() {
-    const rows = 3;
-    const columns = 3;
+  const rows = 3;
+  const columns = 3;
 
-    const board = [];
+  const board = [];
 
-    const createBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board[i] = [];
-            for (let j = 0; j < columns; j++) {
-                board[i].push(" ");
-            }
-        }
+  const createBoard = () => {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(" ");
+      }
     }
+  };
 
-    const log = () => {
-        console.table(board);
-    }
+  const log = () => {
+    console.table(board);
+  };
 
-    return {createBoard, log};
+  return { createBoard, log };
 }
 
 function GameController() {
-    const players = [
-        {
-            name: "Player One",
-            token: "X"
-        },
-        {
-            name: "Player Two",
-            token: "O"
-        }
-    ];
+  const players = [
+    {
+      name: "Player One",
+      token: "X",
+    },
+    {
+      name: "Player Two",
+      token: "O",
+    },
+  ];
 
-    const board = Gameboard();
-    board.createBoard();
-
-
-
+  const board = Gameboard();
+  board.createBoard();
 }
-
-const board = Gameboard();
-
-board.log();
-
