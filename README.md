e3187ed8548fc4f6aa0ebecfa02e498f61931da6
ProtoJanusLabs <165827599+ProtoJanusLabs@users.noreply.github.com>
2024-04-13 19:12:30 -0500

    Finalize project with updated UI,and game logic being completed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tic-tac-toe
+# About
+This is a simple Tic - Tac - Toe game created in vanilla javascript, HTML, and CSS. It's local co-op only however the game supports multiple play attempts. Instructions on how to play this game are found [here](https://en.wikipedia.org/wiki/Tic-tac-toe).
+## Instructions
+Just click on the play button and the game will start with player one represented by the X character and after clicking on an open square the turns will go back and forth from player one and player two until the game ends with a winner or a tie, after which a popup will appear and display the game result and an option to reset the game back to its default state.
+### Attribution
+chalk board overlay from [here](https://www.freepik.com/free-photo/black-rustic-blank-chalkboard-background_15653519.htm#query=chalk%20overlay&position=3&from_view=keyword&track=ais&uuid=ce909b82-66f1-4189-8a5e-bfd4e435bb90)
--- index.js
+++ index.js
@@ -13,19 +13,38 @@ function Gameboard() {
     }
   };
 
-  const render = () => {
+  // this will render each time the board state changes
+  const render = (activePlayer, playRound) => {
     const gameGrid = document.getElementById("gameGrid");
+    gameGrid.innerHTML = "";
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns; j++) {
         const gameSquare = document.createElement("div");
+        const text = document.createElement("p");
+        text.classList.add("gameSquareText");
+        text.textContent = board[i][j];
+        gameSquare.classList.add("gameSquare");
+
+        // this event listener replaces the text content with the player token
+        // and then calls playRound to do the checks for winner and the rest of the
+        // round checks
+        gameSquare.addEventListener("click", function () {
+          if (gameGrid.classList.contains("playing") && board[i][j] === " ") {
+            text.textContent = activePlayer.token;
+            board[i][j] = activePlayer.token;
+            playRound();
+          }
+        });
+        gameSquare.appendChild(text);
         gameGrid.appendChild(gameSquare);
       }
     }
   };
 
-  return { createBoard, render };
+  return { createBoard, render, board };
 }
 
+// within is the logic behid the actual game itself
 function GameController() {
   const players = [
     {
@@ -38,16 +57,133 @@ function GameController() {
     },
   ];
 
+  const board = Gameboard();
+  const infoPanel = document.getElementById("infoPanel");
   let activePlayer = players[0];
 
   const switchActivePlayer = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    infoPanel.innerHTML = `<p>${activePlayer.name} turn<p/>`;
   };
 
-  const board = Gameboard();
-  board.createBoard();
+  const playRound = () => {
+    // play round will be called after the player token is put in place in the 2D Array
+    // so the first thing done is a check to see if there was a winner or a tie
+    if (
+      (board.board[0][0] === "X" &&
+        board.board[0][1] === "X" &&
+        board.board[0][2] === "X") ||
+      (board.board[1][0] === "X" &&
+        board.board[1][1] === "X" &&
+        board.board[1][2] === "X") ||
+      (board.board[2][0] === "X" &&
+        board.board[2][1] === "X" &&
+        board.board[2][2] === "X") ||
+      (board.board[0][0] === "X" &&
+        board.board[1][0] === "X" &&
+        board.board[2][0] === "X") ||
+      (board.board[0][1] === "X" &&
+        board.board[1][1] === "X" &&
+        board.board[2][1] === "X") ||
+      (board.board[0][2] === "X" &&
+        board.board[1][2] === "X" &&
+        board.board[2][2] === "X") ||
+      (board.board[0][0] === "X" &&
+        board.board[1][1] === "X" &&
+        board.board[2][2] === "X") ||
+      (board.board[0][2] === "X" &&
+        board.board[1][1] === "X" &&
+        board.board[2][0] === "X") ||
+      (board.board[0][0] === "O" &&
+        board.board[0][1] === "O" &&
+        board.board[0][2] === "O") ||
+      (board.board[1][0] === "O" &&
+        board.board[1][1] === "O" &&
+        board.board[1][2] === "O") ||
+      (board.board[2][0] === "O" &&
+        board.board[2][1] === "O" &&
+        board.board[2][2] === "O") ||
+      (board.board[0][0] === "O" &&
+        board.board[1][0] === "O" &&
+        board.board[2][0] === "O") ||
+      (board.board[0][1] === "O" &&
+        board.board[1][1] === "O" &&
+        board.board[2][1] === "O") ||
+      (board.board[0][2] === "O" &&
+        board.board[1][2] === "O" &&
+        board.board[2][2] === "O") ||
+      (board.board[0][0] === "O" &&
+        board.board[1][1] === "O" &&
+        board.board[2][2] === "O") ||
+      (board.board[0][2] === "O" &&
+        board.board[1][1] === "O" &&
+        board.board[2][0] === "O")
+    ) {
+      // if there was a winner send over the winner info
+      endGame(activePlayer);
+    } else if (
+      board.board[0][0] !== " " &&
+      board.board[0][1] !== " " &&
+      board.board[0][2] !== " " &&
+      board.board[1][0] !== " " &&
+      board.board[1][1] !== " " &&
+      board.board[1][2] !== " " &&
+      board.board[2][0] !== " " &&
+      board.board[2][1] !== " " &&
+      board.board[2][2] !== " "
+    ) {
+      // if there was a tie, send over the tie info
+      endGame({ name: "tie" });
+    } else {
+      // if no win or tie, then switch to the next player and then rerender the board
+      switchActivePlayer();
+      board.render(activePlayer, playRound);
+    }
+  };
+
+  const endGame = (activePlayer) => {
+    // here we show the modal at the end of the game, and change the result to what happened
+    const modal = document.querySelector("[data-modal]");
+    const gameResult = document.getElementById("gameResult");
+    const resetButton = document.querySelector("[data-close-modal]");
+    modal.showModal();
+
+    if (activePlayer.name !== "tie") {
+      gameResult.textContent = `${activePlayer.name} wins!`;
+    } else {
+      gameResult.textContent = `It was a tie!`;
+    }
+    resetButton.addEventListener("click", () => {
+      modal.close();
+      gameInitialize();
+    });
+  };
+
+  const gameInitialize = () => {
+    // This function resets the game to its default state for future play sessions
+    const gameGrid = document.getElementById("gameGrid");
+    const playButton = document.createElement("button");
+
+    // init values
+    gameGrid.classList.remove("playing");
+    playButton.setAttribute("id", "playButton");
+    playButton.textContent = "Play";
+    infoPanel.innerHTML = "";
+    infoPanel.appendChild(playButton);
+    activePlayer = players[0];
+
+    // create the board from scratch and rerender it
+    board.createBoard();
+    board.render(activePlayer, playRound);
+
+    // when play is clicked start the game
+    playButton.addEventListener("click", function () {
+      gameGrid.classList.add("playing");
+      infoPanel.innerHTML = `<p>${activePlayer.name} turn<p/>`;
+    });
+  };
+
+  gameInitialize();
 }
 
-const board = Gameboard();
-board.createBoard();
-board.render();
+GameController();
